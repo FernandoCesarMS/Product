18aa0f286ec507df1f5e99aca9b33b413da7fcfa
Fernando Cesar <cmnando.silva@gmail.com>
2020-05-23 17:33:48 -0300

    Classes criadas

---
+++ Product/Entities/ImportedProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name,price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public sealed override string Pricetag()
+        {
+            return base.Pricetag();
+        }
+    }
+}
---
+++ Product/Entities/UsedProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+        public UsedProduct(string name,double price, DateTime manufactureDate) : base(name,price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+        public sealed override string Pricetag()
+        {
+            return base.Pricetag();
+        }
+    }
+}
